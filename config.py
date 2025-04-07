9a8d3e54df34b3f8eeeae0c1e32f5feb64d2598d
frea45 <110092585+frea45@users.noreply.github.com>
2025-04-07 11:43:47 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 from pyrogram import Client
 
-API_ID = 123456
-API_HASH = "your_api_hash"
-BOT_TOKEN = "your_bot_token"
+API_ID = 3335796 
+API_HASH = "138b992a0e672e8346d8439c3f42ea78"
+BOT_TOKEN = "7136875110:AAFzyr2i2FbRrmst1sklkJPN7Yz2rXJvSew"
 
-LOG_CHANNEL = -1001234567890
+LOG_CHANNEL = -1001792962793
 
-MONGO_URL = "mongodb://localhost:27017"
+MONGO_URL = "mongodb+srv://abirhasan2005:abirhasan@cluster0.i6qzp.mongodb.net/cluster0?retryWrites=true&w=majority"
 DB_NAME = "link_to_file_bot"
 
 bot = Client(
